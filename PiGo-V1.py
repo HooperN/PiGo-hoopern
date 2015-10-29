b99c948fcb027cd5262373c76f9d9f1bcff56dcd
Nick Hooper <nickhooper@me.com>
2015-10-29 10:55:22 -0400

    More Rover Stuff

--- PiGo-V1.py
+++ PiGo-V1.py
@@ -1,9 +1,27 @@
-#from gopigo import *
-__author__ = 'Nick'
+from gopigo import *
+import time
 
 class Pigo:
+
+    isMoving = False
+    servopos = 90
 
     def __init__(self):
         print "I've gained sentience; hello world"
 
+    def stop(self):
+        self.isMoving = False
+        while stop()!= 1:
+            time.sleep(1)
+            print "My velocity is > 0 and refuses to decrease"
+
+    def fwd(self):
+        self.isMoving = True
+        while fwd() != 1:
+            time.sleep(1)
+            print "I've fallen, and I can't get up!"
+
 beatrix = Pigo()
+beatrix.fwd()
+time.sleep(2)
+beatrix.stop()
