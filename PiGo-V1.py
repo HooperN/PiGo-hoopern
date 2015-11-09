5d51eda34e9264cd588394b094e5155906ecb12f
Nick Hooper <nickhooper@me.com>
2015-11-09 11:44:12 -0500

    Added methods and also a new file

--- PiGo-V1.py
+++ PiGo-V1.py
@@ -1,6 +1,8 @@
 from gopigo import *
 import time
 
+STOP_DIST = 40
+
 class Pigo:
 
     ################################
@@ -8,16 +10,16 @@ class Pigo:
     ################################
 
     status = {"ismoving" : False, "servopos" : 90, "leftspeed" : 150,
-              "rightspeed" : 150}
+              "rightspeed" : 150, 'dist' : 100}
 
     def __init__(self):
         print "I've gained sentience; hello world"
+        self.checkDist()
 
     def stop(self):
-        self.isMoving = False
-        while stop()!= 1:
-            time.sleep(1)
-            print "My velocity is > 0 and refuses to decrease"
+        self.status["ismoving"] = False
+        print "My velocity is > 0 and refuses to decrease"
+        for z in range()
 
     def fwd(self):
         self.isMoving = True
@@ -25,16 +27,39 @@ class Pigo:
             time.sleep(1)
             print "I've fallen, and I can't get up!"
 
+    def keepGoing(self):
+        if self.status['dist'] < STOP_DIST:
+            return False
+        else:
+            return True
+
+    def checkDist(self):
+         self.status['dist'] = us_dist(15)
+         print "Object sighted " + str(self.status['dist']) + " millimeters away!"
+        if not self.keepgoing():
+            print "STOPPING FOR CHECK"
+            self.stop()
+
+    def dance(self):
+        print "Dance fever!"
+         if self.keepgoing():
+             self.circleLeft()
+             self.CircleRight()
+             self.fwdBck()
+             self.fullHeadturn()
+             self.strobe()
+
     ########################
     ### ADVANCED METHODS ###
     ########################
-
 
     ############################
     ### FULL APP STARTS HERE ###
     ############################
 
 beatrix = Pigo()
-beatrix.fwd()
-time.sleep(2)
+while beatrix.keepGoing():
+    beatrix.fwd()
+    time.sleep(2)
+
 beatrix.stop()
---
+++ allstop.py
@@ -0,0 +1,7 @@
+
+from gopigo import *
+
+for x in range(5):
+    stop()
+
+disable_servo()
