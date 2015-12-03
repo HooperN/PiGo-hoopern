9927e37396cedabff1ed2381b04cc9a398e8cb3e
Nick Hooper <nickhooper@me.com>
2015-12-03 11:28:25 -0500

    Additions

--- PiGo-V1.py
+++ PiGo-V1.py
@@ -1,9 +1,11 @@
 #GOPIGO AUTONOMOUS, INSTANTIATED CLASS
-#http://www.dexterindustries.com/GoPiGo/programming/python-programming-for-
-# the-raspberry-pi-gopigo/
+#http://www.dexterindustries.com/GoPiGo/programming/python-programming-for-the-raspberry-pi-gopigo/
 
 from gopigo import *
 import time
+import sys
+from collections import Counter
+import math
 
 STOP_DIST = 20
 
@@ -16,6 +18,7 @@ class Pigo:
     status = {"ismoving" : False, "servopos" : 90, "leftspeed" : 150,
               "rightspeed" : 150, 'dist' : 100}
 
+    free = [0] * 180
 
     def __init__(self):
         print "I've gained sentience; hello world"
@@ -28,15 +31,17 @@ class Pigo:
             stop()
 
 
-    def fwd(self):
-        self.status["ismoving"] = True
+    def moveIt(self):
+        if self.checkDist() = True:
+            fwd()
+            time.sleep(2)
+            self.checkDist()
         while fwd() == -1:
             time.sleep(1)
             print "I've fallen, and I can't get up!"
 
-    #Checks to see if conditions are safe to continue operating
-    def keepGoing(self):
-        if self.status['dist'] < STOP_DIST:
+    def checkDist(self):
+        if us_dist(15) < STOP_DIST:
             print "Road block ahead, get down!"
             return False
         elif volt() > 14 or volt() < 6:
@@ -45,16 +50,9 @@ class Pigo:
         else:
             return True
 
-    def checkDist(self):
-         self.status['dist'] = us_dist(15)
-         print "Object sighted " + str(self.status['dist']) + " millimeters away!"
-         if not self.keepGoing():
-            print "STOPPING FOR CHECK"
-            self.stop()
-
     def dance(self):
         print "Dance fever!"
-       # self.circleLeft()
+        self.circleLeft()
         self.servoSweep()
         self.stop()
         self.circleRight()
@@ -72,9 +70,10 @@ class Pigo:
     ########################
 
     def autoPilot(self):
-        self.fwd()
-        while self.keepGoing():
+        self.moveIt()
+        while True:
             self.checkDist()
+            time.sleep(5)
         self.stop()
 
     def servoSweep(self):
@@ -85,7 +84,7 @@ class Pigo:
             self.checkDist()
 
     def circleLeft(self):
-        if self.keepGoing:
+        if self.checkDist():
             thyme = time.clock()
             if time.clock() < 10 + thyme:
                 print "Left Circle!"
@@ -125,7 +124,7 @@ class Pigo:
         '''
 
     def fwdBwd(self):
-        if self.keepGoing:
+        if self.checkDist():
             print "Forwards, Then Backwards!"
             thyme = time.clock()
             if time.clock() < thyme + 10:
@@ -148,6 +147,8 @@ class Pigo:
         time.sleep(1)
 
 
+
+
     def strobe(self):
         i = 0
         print "Strobe light!"
@@ -161,6 +162,73 @@ class Pigo:
             led_off(0)
             i += 1
 
+    def findPathLeft(self):
+
+
+    def findPathRight(self):
+        dist = us_dist(15)
+        while dist < STOP_DIST:
+            stop()
+            bwd()
+            time.sleep(.5)
+            stop()
+            right_rot()
+            time.sleep(.2)
+            stop()
+            print "Dist:",dist,'cm'
+            return True
+        print "Path is now clear, I think."
+
+
+    def trot(self):   #method to adjust the forward speed
+        set_left_speed(180)
+        set_right_speed(180)
+        fwd()
+
+    def run(self):
+        print "Press ENTER to start"
+        raw_input()				#Wait for input to start
+        self.trot()					#Start moving
+        while True:
+            dist=us_dist(15)			#Find the distance of the object in front
+            print "Dist:",dist,'cm'
+            if dist < STOP_DIST:	#If the object is closer than the "STOP_DIST" distance, stop the GoPiGo
+                print "Something in my way. Going to look for a new path"
+                stop()					#Stop the GoPiGo
+                self.findPathRight()
+            else:
+                print "Let's hit the road again."
+                self.trot()
+                time.sleep(.5)
+
+    def servoCheck(self):
+        for ang in range(60, 120, 5):
+            free[ang] = us_dist(15)
+
+    def pathing(self):
+        self.servoCheck()
+        for free[ang] < STOP_DIST:
+
+    def turnTo(self):
+        
+
+
+
+
+    def avoidance(self):
+        while True:
+            if self.checkDist():
+                self.autoPilot()
+            else:
+                self.servoCheck()
+                if self.pathing:
+                    self.turnTo(self.findAngle())
+                else:
+                    self.reverse()
+
+
+
+
 
     ############################
     ### FULL APP STARTS HERE ###
@@ -168,6 +236,6 @@ class Pigo:
 
 beatrix = Pigo()
 
-beatrix.dance()
+beatrix.
 
 beatrix.stop()
